e3659dd94100f5e298241dd0de13d12dc4eb7b10
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-01-05 15:03:45 +0700

    Add SerialPort settings

--- mcslib-common/src/types.rs
+++ mcslib-common/src/types.rs
@@ -1,8 +1,14 @@
-use crate::serialport::SerialPortSettings;
+use crate::serialport::{
+    DataBits as SPDataBits, FlowControl as SPFlowControl, Parity as SPParity, SerialPortSettings as SPSettings,
+    StopBits as SPStopBits,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str as from_string, to_string, to_string_pretty as to_json, Error as JsonError};
 use std::fmt::{Display, Formatter, Result as FormatterResult};
 use std::net::SocketAddrV4;
+use std::time::Duration;
+
+pub type SerialPortName = String;
 
 const JSON_MAPPING_ERROR_MESSAGE: &str = "Possible JSON mapping failure!";
 
@@ -23,9 +29,61 @@ where
     }
 }
 
+#[repr(u32)]
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum Baud {
+    B4800 = 4_800,
+    B9600 = 9_600,
+    B19200 = 19_200,
+    B38400 = 38_400,
+    B57600 = 57_600,
+    B115200 = 115_200,
+}
+
+#[repr(u8)]
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum DataBits {
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum FlowControl {
+    None,
+    Software,
+    Hardware,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum Parity {
+    None,
+    Odd,
+    Even,
+}
+
+#[repr(u8)]
+#[derive(Deserialize, Serialize, Clone, Debug, Copy)]
+pub enum StopBits {
+    One = 1,
+    Two = 2,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct SerialPortSettings {
+    pub name: SerialPortName,
+    pub baud_rate: Baud,
+    pub data_bits: DataBits,
+    pub flow_control: FlowControl,
+    pub parity: Parity,
+    pub stop_bits: StopBits,
+    pub timeout: Duration,
+}
+
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub enum ServerType {
-    Serial(SerialPortSettings),
+    SerialPort(SerialPortSettings),
     UDP(SocketAddrV4),
     TCP(SocketAddrV4),
 }
@@ -93,6 +151,69 @@ pub struct TrackersIGServerConfig {
     pub server_type: ServerType,
 }
 
+impl Into<SPDataBits> for DataBits {
+    fn into(self) -> SPDataBits {
+        match self {
+            DataBits::Five => SPDataBits::Five,
+            DataBits::Six => SPDataBits::Six,
+            DataBits::Seven => SPDataBits::Seven,
+            DataBits::Eight => SPDataBits::Eight,
+        }
+    }
+}
+
+impl Into<SPFlowControl> for FlowControl {
+    fn into(self) -> SPFlowControl {
+        match self {
+            FlowControl::None => SPFlowControl::None,
+            FlowControl::Software => SPFlowControl::Software,
+            FlowControl::Hardware => SPFlowControl::Hardware,
+        }
+    }
+}
+
+impl Into<SPParity> for Parity {
+    fn into(self) -> SPParity {
+        match self {
+            Parity::None => SPParity::None,
+            Parity::Odd => SPParity::Odd,
+            Parity::Even => SPParity::Even,
+        }
+    }
+}
+
+impl Into<SPStopBits> for StopBits {
+    fn into(self) -> SPStopBits {
+        match self {
+            StopBits::One => SPStopBits::One,
+            StopBits::Two => SPStopBits::Two,
+        }
+    }
+}
+
+impl SerialPortSettings {
+    pub fn get_sp_settings(&self) -> (SerialPortName, SPSettings) {
+        (
+            self.name.clone(),
+            SPSettings {
+                baud_rate: self.baud_rate as u32,
+                data_bits: self.data_bits.into(),
+                flow_control: self.flow_control.into(),
+                parity: self.parity.into(),
+                stop_bits: self.stop_bits.into(),
+                timeout: self.timeout,
+            },
+        )
+    }
+}
+
+impl JsonSerializable<'_> for Baud {}
+impl JsonSerializable<'_> for DataBits {}
+impl JsonSerializable<'_> for FlowControl {}
+impl JsonSerializable<'_> for Parity {}
+impl JsonSerializable<'_> for StopBits {}
+impl JsonSerializable<'_> for SerialPortSettings {}
+impl JsonSerializable<'_> for ServerType {}
 impl JsonSerializable<'_> for SafePoint2D {}
 impl JsonSerializable<'_> for SafePoint3D {}
 impl JsonSerializable<'_> for SafeEulerAngles {}
@@ -101,6 +222,48 @@ impl JsonSerializable<'_> for BaseStationsConfig {}
 impl JsonSerializable<'_> for TrackerEndpoint {}
 impl JsonSerializable<'_> for TrackersConfig {}
 impl JsonSerializable<'_> for TrackersIGServerConfig {}
+
+impl Display for Baud {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for DataBits {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for FlowControl {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for Parity {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for StopBits {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for SerialPortSettings {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Display for ServerType {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
+        write!(formatter, "{}", self.to_json())
+    }
+}
 
 impl Display for SafePoint2D {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
@@ -147,6 +310,56 @@ impl Display for TrackersConfig {
 impl Display for TrackersIGServerConfig {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatterResult {
         write!(formatter, "{}", self.to_json())
+    }
+}
+
+impl Default for Baud {
+    fn default() -> Baud {
+        Baud::B115200
+    }
+}
+
+impl Default for DataBits {
+    fn default() -> DataBits {
+        DataBits::Eight
+    }
+}
+
+impl Default for FlowControl {
+    fn default() -> FlowControl {
+        FlowControl::None
+    }
+}
+
+impl Default for Parity {
+    fn default() -> Parity {
+        Parity::None
+    }
+}
+
+impl Default for StopBits {
+    fn default() -> StopBits {
+        StopBits::Two
+    }
+}
+
+impl Default for SerialPortSettings {
+    fn default() -> SerialPortSettings {
+        SerialPortSettings {
+            name: "/dev/ttyS0".into(),
+            baud_rate: Default::default(),
+            data_bits: Default::default(),
+            parity: Default::default(),
+            stop_bits: Default::default(),
+            flow_control: Default::default(),
+            timeout: Duration::from_secs(1),
+        }
+    }
+}
+
+impl Default for ServerType {
+    fn default() -> ServerType {
+        ServerType::SerialPort(Default::default())
     }
 }
 
@@ -208,8 +421,8 @@ impl Default for TrackersConfig {
 impl Default for TrackersIGServerConfig {
     fn default() -> TrackersIGServerConfig {
         TrackersIGServerConfig {
-            name: "",
-            server_type: ServerType::TCP("127.0.0.1:4000".into()),
+            name: "".into(),
+            server_type: ServerType::TCP("127.0.0.1:4000".parse().unwrap()),
         }
     }
 }
