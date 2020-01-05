28c8d08b955de441ed466633c4ae229827bc6758
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-01-05 21:27:17 +0700

    Hook panic to use default logger

--- mcsapp-motion-tracker/src/main.rs
+++ mcsapp-motion-tracker/src/main.rs
@@ -9,13 +9,21 @@ mod networks;
 use mcslib_common::config::{init_configs, load_configs};
 use mcslib_common::init_log;
 use std::io::Result as IOResult;
+use std::panic::{set_hook, PanicInfo};
 
 const STATIONS_CONFIG_PATH: &str = "base-stations.config";
 const TRACKERS_CONFIG_PATH: &str = "motion-trackers.config";
 const APP_CONFIG_PATH: &str = "app.config";
 
-fn main() -> IOResult<()> {
+fn init_logging() {
     init_log(true);
+    set_hook(Box::new(|panic_info| {
+        error!("ABORT! {}", panic_info);
+    }));
+}
+
+fn main() -> IOResult<()> {
+    init_logging();
     let _ = init_configs(STATIONS_CONFIG_PATH, TRACKERS_CONFIG_PATH, APP_CONFIG_PATH);
     let (base_stations_config, motion_trackers_config, app_config) =
         load_configs(STATIONS_CONFIG_PATH, TRACKERS_CONFIG_PATH, APP_CONFIG_PATH)?;
